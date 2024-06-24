6499e4a8f8b42062636298656dee0f0560709142
Danny Krämer <danny-kraemer@outlook.de>
2024-06-24 07:48:20 +0200

    ch 2 done

--- main.go
+++ main.go
@@ -5,21 +5,23 @@ import (
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
 func main() {
 	const port = "8080"
+	apiCfg := apiConfig{
+		fileserverHits: 0,
+	}
 	mux := http.NewServeMux()
-	mux.Handle("/app/*", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-
-	mux.HandleFunc("/healthz", handler)
+	mux.Handle("/app/*", apiCfg.middlewarMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
 	server := &http.Server{Addr: ":" + port, Handler: mux,}
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
 
-}
-
-func handler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))	
 }
